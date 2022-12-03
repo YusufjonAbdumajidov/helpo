e37bcdd0d1b101c5819e0258be9a738f7e8f2a6b
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2022-12-03 18:57:50 +0100

    new functionalities added

--- src/Components/Calculator.jsx
+++ src/Components/Calculator.jsx
@@ -1,33 +1,50 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import buttons from '../data/calculator';
-import { calculate } from '../features/calculator/calculatorSlice';
+import React, { } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addDigit, clear, chooseOperation, deleteDigit, calculate } from '../features/calculator/calculatorSlice';
 
 
+const INTEGER_FORMATTER = new Intl.NumberFormat("en-us", {
+  maximumFractionDigits: 0,
+})
+const formatOperand = (operand) =>{
+  if(operand == null) return;
+  const [ integer, decimal ] = operand.split(".");
+  if(decimal == null) return INTEGER_FORMATTER.format(integer);
+  return `${INTEGER_FORMATTER.format(integer)}.${decimal}`
+}
 
 const Calculator = () => {
   const dispatch = useDispatch();
-  const [ nums, setNums ] = useState({
-    num1: 0,
-    num2: 0,
-    operator: '',
-  });
-  // const arithmetic = operator =>{
-  //   typeof operator === "string" ? setNums()
-  // }
-  console.log(nums)
+  const { currentOperand, previousOperand, operation } = useSelector((store)=> store.calculator);
   
   return <div className="calculatorContainer">
     <h1>Calculator</h1>
     <div className="calculator">
-      <section className="display">0</section>
+      <section className="display">
+        <div className="previous-operand">{formatOperand(previousOperand)} {operation}</div>
+        <div className="current-operand">{formatOperand(currentOperand)}</div>
+      </section>
       <div className="calButtons">
-          {buttons.map((button) => {
-            const { id, type } = button;
-            return <section
-            onClick={() => dispatch(calculate(type))}
-             className={type} key={id}>{type}</section>
-          })}
+        
+          <section onClick={() => dispatch(deleteDigit())}>del</section>
+          <section onClick={() => dispatch(chooseOperation('%'))}>%</section>
+          <section onClick={()=> dispatch(clear())}>ac</section>
+          <section onClick={()=> dispatch(addDigit(7))}>7</section>
+          <section onClick={()=> dispatch(addDigit(8))}>8</section>
+          <section onClick={()=> dispatch(addDigit(9))}>9</section>
+          <section onClick={() => dispatch(chooseOperation('÷'))}>÷</section>
+          <section onClick={()=> dispatch(addDigit(4))}>4</section>
+          <section onClick={()=> dispatch(addDigit(5))}>5</section>
+          <section onClick={()=> dispatch(addDigit(6))}>6</section>
+          <section onClick={() => dispatch(chooseOperation('x'))}>x</section>
+          <section onClick={()=> dispatch(addDigit(1))}>1</section>
+          <section onClick={()=> dispatch(addDigit(2))}>2</section>
+          <section onClick={()=> dispatch(addDigit(3))}>3</section>
+          <section onClick={() => dispatch(chooseOperation('-'))}>-</section>
+          <section onClick={()=> dispatch(addDigit(0))}>0</section>
+          <section onClick={()=> dispatch(addDigit("."))}>.</section>
+          <section  onClick={()=> dispatch(calculate())}>=</section>
+          <section onClick={() => dispatch(chooseOperation('+'))} >+</section>
       </div>
     </div>
   </div>
--- src/features/calculator/calculatorSlice.jsx
+++ src/features/calculator/calculatorSlice.jsx
@@ -2,19 +2,133 @@ import { createSlice } from "@reduxjs/toolkit";
 
 
 const initialState = {
-    number: 0,
+    currentOperand: null,
+    previousOperand: null,
+    operation: '',
+    overwrite: false,
 };
+
+const evaluate = ({ currentOperand, previousOperand, operation }) => {
+  const prev = parseFloat(previousOperand)
+  const current = parseFloat(currentOperand)
+  if (isNaN(prev) || isNaN(current)) return ""
+  let computation = ""
+  switch (operation) {
+    case "+":
+      computation = prev + current
+      break
+    case "-":
+      computation = prev - current
+      break
+    case "x":
+      computation = prev * current
+      break
+    case "÷":
+      computation = prev / current
+      break
+    case "%":
+      computation = ((current / prev) * 100)
+      break
+    default: console.log("Error in operation");
+  }
+
+  return computation.toString()
+}
+
+
 
 const calculatorSlice = createSlice({
     name: "calculator",
     initialState,
     reducers: {
-        calculate: (state, {payload} ) =>{
-            console.log(payload);
+        addDigit: (state, { payload }) => {
+            if(state.overwrite){
+                return{
+                    ...state,
+                    currentOperand: payload.digit,
+                    overwrite: false,
+                }
+            }
+            if(payload === '0' && state.currentOperand === "0"){
+                return state;
+            }
+            if (payload === "." && state.currentOperand.includes(".")) {
+                return state
+            }
+            return {
+                ...state,
+                currentOperand: `${state.currentOperand || ""}${payload}`,
+            }
+        },
+        clear: () => {
+            return  {
+                currentOperand: null,
+                previousOperand: null,
+                operation: '',
+            }
+        },
+        chooseOperation: (state, { payload }) => {
+            if (state.currentOperand == null && state.previousOperand == null) {
+                return state
+            }
+            if (state.currentOperand == null) {
+                return {
+                  ...state,
+                  operation: payload,
+                }
+            }
+            if (state.previousOperand == null) {
+                return {
+                  ...state,
+                  operation: payload,
+                  previousOperand: state.currentOperand,
+                  currentOperand: null,
+                }
+            }
+            return {
+                ...state,
+                previousOperand: evaluate(state),
+                operation: payload,
+                currentOperand: null,
+            }
+        },
+        deleteDigit: (state)=> {
+            if(state.overwrite){
+                return{
+                    ...state,
+                    overwrite: false,
+                    currentOperand: null,
+                }
+            }
+            if (state.currentOperand == null) return state;
+            if (state.currentOperand.length === 1) {
+              return { ...state, currentOperand: null }
+            }
+            return {
+                ...state,
+                currentOperand: state.currentOperand.slice(0, -1),
+            }
+        },
+        calculate: (state) => {
+            if(
+                state.operation == null ||
+                state.currentOperand == null ||
+                state.previousOperand == null
+            ){
+                return state
+            }
+            return {
+                ...state,
+                overwrite: true,
+                previousOperand: null,
+                operation: null,
+                currentOperand: evaluate(state),
+            }
         }
     },
 });
 
-export const { calculate } = calculatorSlice.actions;
+export const { addDigit, clear, 
+    chooseOperation, deleteDigit, calculate } = calculatorSlice.actions;
 
 export default calculatorSlice.reducer;
