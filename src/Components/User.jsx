71c351beefcea4b0ce49f9ac29e9c1d716003eaf
webdesigner.uz <76707414+webdesignerUZ@users.noreply.github.com>
2022-12-29 18:22:52 +0300

    new functionality

--- src/Components/User.jsx
+++ src/Components/User.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react';
+
+
 
 const User = () => {
+  const [name, setName] = useState("");
+  const [major, setMajor] = useState("");
+  const [stdID, setStdID] = useState("");
+  const [studentList, setStudentList] = useState([]);
+
+  const handleSubmit = e =>{
+    e.preventDefault();
+    if(name.length > 0 && major.length > 0 && stdID.length > 0){
+  
+      const newItem = {id: new Date().getTime().toString(), studentName: name, studentMajor: major, studentID: stdID};
+      setStudentList([...studentList, newItem]);
+      
+      setName("");
+      setMajor("");
+      setStdID("");
+    }
+    
+  }
   return (
-    <div>User</div>
+    <div className='userContainer'>
+      <h1>User Information</h1>
+      <form action="#" onSubmit={handleSubmit}>
+        <input type="text" name="fullname" id="fullname" placeholder=' your full name' value={name} onChange={(e)=> setName(e.target.value)}/>
+        <input type="text" placeholder='major' value={major} onChange={(e)=> setMajor(e.target.value)}/>
+        <input type="text" placeholder='you ID' value={stdID} onChange={(e)=> setStdID(e.target.value)}/>
+        <button >submit</button>
+      </form>
+
+    
+      {studentList.length > 0 ?  <div className="userInfo">
+        {studentList.map((item)=> {
+          console.log(item)
+          return <section key={item.studentID}>
+            <h2>Student Information</h2>
+            <h3>Full Name: {item.studentName}</h3>
+            <p>Major: {item.studentMajor}</p>
+            <p>ID: {item.studentID}</p>
+          </section>
+        })}
+      </div> : <p>Nothing added yet</p>}
+      
+    </div>
   )
 }
 
